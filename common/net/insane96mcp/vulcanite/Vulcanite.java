3626e5d223e2fd059f8f26ba7b29860573475cdc
Alberto Del Villano <insane96mcp@gmail.com>
2017-08-19 19:33:07 +0200

    Release 1.0.2

--- common/net/insane96mcp/vulcanite/Vulcanite.java
+++ common/net/insane96mcp/vulcanite/Vulcanite.java
@@ -15,7 +15,7 @@ public class Vulcanite {
 	
 	public static final String MOD_ID = "vulcanite";
 	public static final String MOD_NAME = "Vulcanite";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.0.2";
 	public static final String RESOURCE_PREFIX = MOD_ID.toLowerCase() + ":";
 
 	public static Random random = new Random();
