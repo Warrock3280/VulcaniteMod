595cb4eac9e7f56dfd869811bf8a6bb334cae42a
Alberto Del Villano <insane96mcp@gmail.com>
2017-08-19 10:04:09 +0200

    Apparently areItemsEqual...(ItemStack, ItemStack) fixed the problem so removing debug code

--- common/net/insane96mcp/vulcanite/lib/CustomEventHandler.java
+++ common/net/insane96mcp/vulcanite/lib/CustomEventHandler.java
@@ -27,28 +27,23 @@ public class CustomEventHandler {
 				if (source == damageSource) {
 					float amount = event.getAmount();
 					
-					ItemStack[] armorList = new ItemStack[] {new ItemStack(ModItems.vulcaniteHelmetItem, 1), new ItemStack(ModItems.vulcaniteChestplateItem, 1), new ItemStack(ModItems.vulcaniteLeggingsItem, 1), new ItemStack(ModItems.vulcaniteBootsItem, 1)};
+					ItemStack[] armorList = new ItemStack[] {new ItemStack(ModItems.vulcaniteHelmetItem), new ItemStack(ModItems.vulcaniteChestplateItem), new ItemStack(ModItems.vulcaniteLeggingsItem), new ItemStack(ModItems.vulcaniteBootsItem)};
 				    int gearCounter = 0;
-			        player.sendMessage(new TextComponentString("Gear counter " + Integer.toString(gearCounter)));
 				    Iterable<ItemStack> playerArmor = player.getArmorInventoryList();
 				    for (ItemStack armorPiece : playerArmor) {
 				    	for (ItemStack armorL : armorList) {
-					        player.sendMessage(new TextComponentString("Checking " + armorPiece.getItem() + " " + armorL.getItem()));
 					        if (ItemStack.areItemsEqualIgnoreDurability(armorPiece, armorL)) {
 								gearCounter++;
-						        player.sendMessage(new TextComponentString("ArmorPiece found"));
 								break;
 							}
 						}
 					}
-			        player.sendMessage(new TextComponentString("Gear counter " + Integer.toString(gearCounter)));
 				    if(gearCounter >= 1) {
 				    	float reductionPerPiece = 20f;
 				    	float percentageReduction = gearCounter * reductionPerPiece;
 				    	amount -= (amount / 100 * percentageReduction);
 				        event.setAmount(amount);
 				    }
-				    player.sendMessage(new TextComponentString("Damage amount " + Float.toString(amount)));
 				}
 			}
 		}
