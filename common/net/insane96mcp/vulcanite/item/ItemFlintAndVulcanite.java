346f5aa9df43b010e4b0d03dcccb32e761ccd752
Alberto Del Villano <insane96mcp@gmail.com>
2017-09-06 11:10:45 +0200

    fix not begin able to lit Tnt with Flint and Vulcanite

--- common/net/insane96mcp/vulcanite/item/ItemFlintAndVulcanite.java
+++ common/net/insane96mcp/vulcanite/item/ItemFlintAndVulcanite.java
@@ -3,11 +3,21 @@ package net.insane96mcp.vulcanite.item;
 import java.util.List;
 
 import net.insane96mcp.vulcanite.Vulcanite;
+import net.minecraft.block.BlockTNT;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.item.EntityTNTPrimed;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemFlintAndSteel;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
 
 public class ItemFlintAndVulcanite extends ItemFlintAndSteel{
 	public ItemFlintAndVulcanite(String name, ToolMaterial material, CreativeTabs tab) {
@@ -24,5 +34,23 @@ public class ItemFlintAndVulcanite extends ItemFlintAndSteel{
 	@Override
 	public void addInformation(ItemStack stack, EntityPlayer player, List<String> tooltip, boolean advanced) {
 		tooltip.add(TextFormatting.GOLD + "Rightclick on mob to set it on fire for " + TextFormatting.RED + "3 seconds");
+	}
+
+	@Override
+	public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand,
+			EnumFacing facing, float hitX, float hitY, float hitZ) {
+		if (worldIn.getBlockState(pos).getBlock() == Blocks.TNT) {
+			if (!worldIn.isRemote)
+			{
+		        EntityTNTPrimed entitytntprimed = new EntityTNTPrimed(worldIn, (double)((float)pos.getX() + 0.5F), (double)pos.getY(), (double)((float)pos.getZ() + 0.5F), player);
+		        worldIn.spawnEntity(entitytntprimed);
+                worldIn.playSound((EntityPlayer)null, entitytntprimed.posX, entitytntprimed.posY, entitytntprimed.posZ, SoundEvents.ENTITY_TNT_PRIMED, SoundCategory.BLOCKS, 1.0F, 1.0F);
+	        }
+            worldIn.setBlockState(pos, Blocks.AIR.getDefaultState(), 11);
+            player.getHeldItem(hand).damageItem(1, player);
+
+            return EnumActionResult.SUCCESS;
+		}
+		return super.onItemUse(player, worldIn, pos, hand, facing, hitX, hitY, hitZ);
 	}
 }
