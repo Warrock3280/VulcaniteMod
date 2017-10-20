000d7a6dca3b72b4b732d5fd1a62f99b5dc5a875
Alberto Del Villano <insane96mcp@gmail.com>
2017-10-20 11:56:27 +0200

    1.2.2 - Added missing files

--- common/net/insane96mcp/vulcanite/Vulcanite.java
+++ common/net/insane96mcp/vulcanite/Vulcanite.java
@@ -16,7 +16,7 @@ public class Vulcanite {
 	
 	public static final String MOD_ID = "vulcanite";
 	public static final String MOD_NAME = "Vulcanite";
-	public static final String VERSION = "1.2.1";
+	public static final String VERSION = "1.2.2";
 	public static final String RESOURCE_PREFIX = MOD_ID.toLowerCase() + ":";
 	
 	public static Configuration config;
