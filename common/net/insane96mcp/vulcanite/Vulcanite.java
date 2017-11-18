dd8bf50505e53d213f780eb62e583dabad4cac6e
Alberto Del Villano <insane96mcp@gmail.com>
2017-11-18 10:03:04 +0100

    Revert "Revert "1.3.0b""
    This reverts commit 550d93d8aadc8aad2a7c118e5dc639c4902fda52.

---
+++ common/net/insane96mcp/vulcanite/Vulcanite.java
@@ -0,0 +1,48 @@
+package net.insane96mcp.vulcanite;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import net.insane96mcp.vulcanite.proxies.CommonProxy;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Vulcanite.MOD_ID, name = Vulcanite.MOD_NAME, version = Vulcanite.VERSION)
+public class Vulcanite {
+	
+	public static final String MOD_ID = "vulcanite";
+	public static final String MOD_NAME = "Vulcanite";
+	public static final String VERSION = "1.3.0c";
+	public static final String RESOURCE_PREFIX = MOD_ID.toLowerCase() + ":";
+	
+	public static Configuration config = new Configuration();
+
+	public static Random random = new Random();
+	
+	@Instance(MOD_ID)
+	public static Vulcanite instance;
+	
+	@SidedProxy(clientSide = "net.insane96mcp.vulcanite.proxies.ClientProxy", serverSide = "net.insane96mcp.vulcanite.proxies.ServerProxy")
+	public static CommonProxy proxy;
+	
+	@EventHandler
+	public void PreInit(FMLPreInitializationEvent event) {
+		proxy.PreInit(event);
+	}
+	
+	@EventHandler
+	public void Init(FMLInitializationEvent event) {
+		proxy.Init(event);
+	}
+	
+	@EventHandler
+	public void PostInit(FMLPostInitializationEvent event) {
+		proxy.PostInit(event);
+	}
+}
