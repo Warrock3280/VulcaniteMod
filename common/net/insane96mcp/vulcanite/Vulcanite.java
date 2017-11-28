872e0828b47880635e7d2d4b623a55b3c4a9e463
Alberto Del Villano <insane96mcp@gmail.com>
2017-11-28 11:21:57 +0100

    1.3.3 - Fixed and updated spanish translations

--- common/net/insane96mcp/vulcanite/Vulcanite.java
+++ common/net/insane96mcp/vulcanite/Vulcanite.java
@@ -18,7 +18,7 @@ public class Vulcanite {
 	
 	public static final String MOD_ID = "vulcanite";
 	public static final String MOD_NAME = "Vulcanite";
-	public static final String VERSION = "1.3.1";
+	public static final String VERSION = "1.3.3";
 	public static final String RESOURCE_PREFIX = MOD_ID.toLowerCase() + ":";
 	
 	public static Configuration config = new Configuration();
