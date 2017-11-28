30323a36bb50bf3c0308aec251001785695e27a7
Alberto Del Villano <insane96mcp@gmail.com>
2017-11-28 11:19:21 +0100

    1.3.3 - Fixed an updated Spanish Translations

--- common/net/insane96mcp/vulcanite/Vulcanite.java
+++ common/net/insane96mcp/vulcanite/Vulcanite.java
@@ -17,7 +17,7 @@ public class Vulcanite {
 	
 	public static final String MOD_ID = "vulcanite";
 	public static final String MOD_NAME = "Vulcanite";
-	public static final String VERSION = "1.3.2";
+	public static final String VERSION = "1.3.3";
 	public static final String RESOURCE_PREFIX = MOD_ID.toLowerCase() + ":";
 	public static final String MINECRAFT_VERSIONS = "[1.12,1.12.2]";
 	
